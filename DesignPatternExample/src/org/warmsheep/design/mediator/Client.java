883694ed82fc0b9ead77d8c97e09d63a1962dc1f
Warmsheep <warmsheep@me.com>
2013-01-07 21:08:04 +0800

    增加客户场景类

---
+++ DesignPatternExample/src/org/warmsheep/design/mediator/Client.java
@@ -0,0 +1,15 @@
+package org.warmsheep.design.mediator;
+
+/**
+ * 客户，场景类
+ * @author Warmsheep
+ * @createTime Jan 7, 2013
+ * @function 
+ *
+ */
+public class Client {
+
+	public static void main(String[] args) {
+		
+	}
+}
